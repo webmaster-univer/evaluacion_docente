ac257416ebcb709ec7f2d64140a3ea2cde6b11e5
nacho <nacho739@hotmail.com>
2021-02-15 18:43:11 -0600

    Migrates y models
    la base que se me ocurre por ahora

--- database/migrations/2021_02_15_181029_create_materias_table.php
+++ database/migrations/2021_02_15_181029_create_materias_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAlumnosTable extends Migration
+class CreateMateriasTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateAlumnosTable extends Migration
      */
     public function up()
     {
-        Schema::create('alumnos', function (Blueprint $table) {
+        Schema::create('materias', function (Blueprint $table) {
             $table->id();
-            $table->string('id_pwc');
-            $table->string('nombre_completo');
-            $table->integer('plantel_id');
+            $table->string('descripcion');
+            $table->integer('alumno_id');
             $table->integer('grupo_id');
-            $table->integer('grado_id');
-            $table->integer('nivel_grado_id');
+            $table->integer('docente_id');
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ class CreateAlumnosTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('alumnos');
+        Schema::dropIfExists('materias');
     }
 }
