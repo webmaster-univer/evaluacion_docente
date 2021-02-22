dded11c58b2820ed6101811fd7259d18fd5f186c
nacho <nacho739@hotmail.com>
2021-02-22 17:45:22 -0600

    Segunda version de consultas

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,9 @@ Route::get('/', function () {
 });
 
 
-Route::get('/proceso', 'App\Http\Controllers\PrincipalController@CrearUsuarios')->name('proceso');
-
+//Route::get('/proceso', 'App\Http\Controllers\PrincipalController@CrearUsuarios')->name('proceso');
+//Route::get('/agregarID', 'App\Http\Controllers\PrincipalController@AgregarIds')->name('AgregarIds');
+//Route::get('/agregarIDgrupos', 'App\Http\Controllers\PrincipalController@AgregarIdsGrupos')->name('AgregarIdsGrupos');
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', 'App\Http\Controllers\PrincipalController@Index')->name('dashboard');
 Route::group(['middleware' => ['auth']], function () {
 Route::resources([
